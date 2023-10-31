8a969304b1a76d84478a810ce84060dee1762f7f
uploads <85631165+21Turtles@users.noreply.github.com>
2023-10-31 10:16:10 -0400

    Add files via upload

---
+++ game1.lua
@@ -0,0 +1,31 @@
+--MicroLua Ds game 01
+
+--Input
+local i = 1
+	for i = 1 do
+local move-up = 0
+local move-down = 0
+local move-left = 0
+local move-right = 0
+	end
+
+	for i = 1 do
+if Keys.newPress.[Up] do
+local move-up = 0
+end
+if Keys.newPress.[Down] do
+local move-down = 0
+end
+if Keys.newPress.[Left] do
+local move-left = 0
+end
+if Keys.newPress.[Right] do
+local move-right = 0
+end
+	end
+
+--Graphics
+local c1 = Color Color.new(5, 20, 5)
+--basic green 
+Void screen.drawFillRect(screen_up, x0, y0, x1, y1, c1)
+
