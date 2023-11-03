e55c43a3219efb1356b5ade778acafd3de47d686
uploads <85631165+21Turtles@users.noreply.github.com>
2023-11-03 16:21:24 -0400

    Update game1.lua

--- game1.lua
+++ game1.lua
@@ -3,24 +3,24 @@
 --Input
 local i = 1
 	for i = 1 do
-local move-up = 0
-local move-down = 0
-local move-left = 0
-local move-right = 0
+local move-up = 0;
+local move-down = 0;
+local move-left = 0;
+local move-right = 0;
 	end
 
 	for i = 1 do
 if Keys.newPress.[Up] do
-local move-up = 0
+local move-up = 0;
 end
 if Keys.newPress.[Down] do
-local move-down = 0
+local move-down = 0;
 end
 if Keys.newPress.[Left] do
-local move-left = 0
+local move-left = 0;
 end
 if Keys.newPress.[Right] do
-local move-right = 0
+local move-right = 0;
 end
 	end
 
